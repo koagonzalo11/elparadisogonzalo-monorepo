9f1134ed1294fcdc9276ce01708773083437450a
azeh agowa <genyoungclip@gmail.com>
2025-07-18 21:47:19 +0100

    🧹 Add .gitignore to exclude junk

---
+++ packages/frontend/vite.config.js
@@ -0,0 +1,11 @@
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+export default {
+  plugins: [wasm(), topLevelAwait()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  }
+};
